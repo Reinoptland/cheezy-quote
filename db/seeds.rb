b66b517c634802dee39c549d95bbeec2f507641d
reinoptland <reinoptland@gmail.com>
2017-10-02 13:24:05 +0200

    Add some seed data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quote_list = [
+  ["Academician Prokhor Zakharov", "Begin with a function of arbitrary complexity. Feed it values, "\
+    "\"sense data\". Then, take your result, square it, and feed it back into your original function" \
+    ", adding a new set of sense data. Continue to feed your results back into the original function" \
+    "ad infinitum. What do you have? The fundamental principle of human consciousness"],
+  ["Napoleon Bonaparte", "Glory is fleeting, but obscurity is forever."],
+  ["Yogi Berra", "In theory, there is no difference between theory and practice. But in practice, there is."],
+  ["Friedrich Nietzsche", "There are no facts, only interpretations."],
+  ["Dwight D. Eisenhouwer", "A people that values its privileges above its principles soon loses both."],
+]
+
+quote_list.each do |source, content|
+  Quote.create( source: source, content: content )
+end
