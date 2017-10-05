e16dc73ff2f3871f1f7f7049d127f123dc815a54
reinoptland <reinoptland@gmail.com>
2017-10-05 14:35:03 +0200

    Add and style navbar partial

--- spec/views/quotes/index.html.erb_spec.rb
+++ spec/views/quotes/index.html.erb_spec.rb
@@ -22,10 +22,9 @@ RSpec.describe "quotes/index", type: :view do
       assert_select "tr>td", :text => "MyText".to_s, :count => 2
     end
 
-    it "when an admin is not logged in it does not show links for edit, new, destroy" do
+    it "when an admin is not logged in it does not show links for edit, destroy" do
       render
       assert_select "tr>td>a", :text => "Edit".to_s, :count => 0
-      assert_select "tr>td>a", :text => "New".to_s, :count => 0
       assert_select "tr>td>a", :text => "Destroy".to_s, :count => 0
     end
   end
@@ -33,12 +32,11 @@ RSpec.describe "quotes/index", type: :view do
   context "Admin authenticated view" do
     login_admin
 
-    it "when an admin is logged it does not shows links for edit, destroy and new quote" do
+    it "when an admin is logged it shows links for edit and destroy quote" do
       render
 
       assert_select "tr>td>a", :text => "Edit".to_s, :count => 2
       assert_select "tr>td>a", :text => "Destroy".to_s, :count => 2
-      assert_select "a", :text => "New Quote".to_s, :count => 1
     end
   end
 
